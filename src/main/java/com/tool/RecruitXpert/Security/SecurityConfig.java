becceb3f2da6330642a335fd6d2b3ba8e1ee25e1
Ashutosh <ashushukla800@gmail.com>
2024-02-12 18:33:31 +0530

    final changes

--- src/main/java/com/tool/RecruitXpert/Security/SecurityConfig.java
+++ src/main/java/com/tool/RecruitXpert/Security/SecurityConfig.java
@@ -73,5 +73,4 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-//ashugi
 }
